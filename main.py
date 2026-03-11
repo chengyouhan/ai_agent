6403c5a84c68af3d5f686212a5f85c9e0953464b
chengyouhan <chengyouhan20090612@gmail.com>
2026-03-11 16:21:57 +0800

    feat: Integrate LangChain's ChatOpenAI for LLM interaction and environment variable loading.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from ai-agent!")
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
 
+
+def main():
+    llm = ChatOpenAI(model="gpt-4o-mini",temperature=0.7)
+    response = llm.invoke("Hello, how are you?")
+    print(response.content)
 
 if __name__ == "__main__":
     main()
